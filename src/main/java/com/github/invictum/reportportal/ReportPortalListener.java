b9e73b9396e19572e53817966d048fef9e8330aa
Aliaksei Boole <aboole@workfusion.com>
2025-06-25 15:13:16 +0200

    Migrate to Java21 and Fix integration issue.

--- src/main/java/com/github/invictum/reportportal/ReportPortalListener.java
+++ src/main/java/com/github/invictum/reportportal/ReportPortalListener.java
@@ -3,20 +3,20 @@ package com.github.invictum.reportportal;
 import com.github.invictum.reportportal.injector.IntegrationInjector;
 import com.github.invictum.reportportal.recorder.TestRecorder;
 import com.google.inject.Inject;
-import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
+import java.util.Map;
 import net.thucydides.model.domain.DataTable;
 import net.thucydides.model.domain.Story;
-import net.thucydides.model.domain.TestOutcome;
-import net.thucydides.model.domain.TestResult;
-import net.thucydides.model.screenshots.ScreenshotAndHtmlSource;
 import net.thucydides.model.steps.ExecutedStepDescription;
 import net.thucydides.model.steps.StepFailure;
 import net.thucydides.model.steps.StepListener;
+import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
+import net.thucydides.model.domain.TestOutcome;
+import net.thucydides.model.domain.TestResult;
+import net.thucydides.model.screenshots.ScreenshotAndHtmlSource;
 import org.openqa.selenium.logging.Logs;
 
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Map;
 
 public class ReportPortalListener implements StepListener {
 
@@ -30,6 +30,7 @@ public class ReportPortalListener implements StepListener {
         IntegrationInjector.getInjector().injectMembers(this);
     }
 
+    @Override
     public void testSuiteStarted(Class<?> storyClass) {
         // Not used by listener
     }
@@ -39,10 +40,12 @@ public class ReportPortalListener implements StepListener {
         // Not used by listener
     }
 
+    @Override
     public void testSuiteFinished() {
         suiteStorage.finalizeActive();
     }
 
+    @Override
     public void testStarted(String description) {
         // Not used by listener
     }
@@ -56,26 +59,29 @@ public class ReportPortalListener implements StepListener {
         // Not used by listener
     }
 
+    @Override
     public void testFinished(TestOutcome result) {
         // Not used by listener
     }
 
     @Override
     public void testFinished(TestOutcome testOutcome, boolean b, ZonedDateTime zonedDateTime) {
-        // Not used by listener
         TestRecorder recorder = TestRecorder.forTest(testOutcome);
         recorder.record(testOutcome);
         logStorage.clean();
     }
 
+    @Override
     public void testRetried() {
         // Not used by listener
     }
 
+    @Override
     public void stepStarted(ExecutedStepDescription description) {
         // Not used by listener
     }
 
+    @Override
     public void skippedStepStarted(ExecutedStepDescription description) {
         // Not used by listener
     }
@@ -89,18 +95,22 @@ public class ReportPortalListener implements StepListener {
 
     }
 
+    @Override
     public void lastStepFailed(StepFailure failure) {
         // Not used by listener
     }
 
+    @Override
     public void stepIgnored() {
         // Not used by listener
     }
 
+    @Override
     public void stepPending() {
         // Not used by listener
     }
 
+    @Override
     public void stepPending(String message) {
         // Not used by listener
     }
@@ -110,55 +120,61 @@ public class ReportPortalListener implements StepListener {
     }
 
     @Override
-    public void stepFinished(List<ScreenshotAndHtmlSource> list) {
-        // Not used by listener
-    }
-
-    @Override
     public void stepFinished(List<ScreenshotAndHtmlSource> screenshotList, ZonedDateTime time) {
         // Not used by listener
     }
 
+    @Override
     public void testFailed(TestOutcome testOutcome, Throwable cause) {
         // Not used by listener
     }
 
+    @Override
     public void testIgnored() {
         // Not used by listener
     }
 
+    @Override
     public void testSkipped() {
         // Not used by listener
     }
 
+    @Override
     public void testPending() {
         // Not used by listener
     }
 
+    @Override
     public void testIsManual() {
         // Not used by listener
     }
 
+    @Override
     public void notifyScreenChange() {
         // Not used by listener
     }
 
+    @Override
     public void useExamplesFrom(DataTable table) {
         // Not used by listener
     }
 
+    @Override
     public void addNewExamplesFrom(DataTable table) {
         // Not used by listener
     }
 
+    @Override
     public void exampleStarted(Map<String, String> data) {
         // Not used by listener
     }
 
+    @Override
     public void exampleFinished() {
         // Not used by listener
     }
 
+    @Override
     public void assumptionViolated(String message) {
         // Not used by listener
     }
@@ -184,4 +200,5 @@ public class ReportPortalListener implements StepListener {
             logStorage.collect(logs);
         }
     }
+
 }
